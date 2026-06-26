baf6f225f220ca12d615c2edcff6fe85de4ea293
selengr <reza1997karbakhsh@gmail.com>
2026-06-26 15:10:03 +0330

    created signup api

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,29 @@
+import { connectDB } from "@/lib/mongodb";
+import { User } from "@/models/User";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+
+    await connectDB();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json({ message: "User already exists" }, { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json({ message: "User successfuly was created", user });
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  }
+}
