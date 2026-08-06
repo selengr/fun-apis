7280f4682dad54dcf7cd56cafda8f268ee34c8f1
selengr <reza1997karbakhsh@gmail.com>
2026-08-06 23:33:05 +0330

    add  author images - fix

--- app/page.tsx
+++ app/page.tsx
@@ -137,7 +137,6 @@ export default function AgenticPage() {
       //       let next = await test.json()
       //       debugger
       //    } catch(error:any){
-      //  debugger
       //    }
     }
 
